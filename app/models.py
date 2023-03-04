c52ecc1576bd7c0eb83404dcc9778fbf04646c1e
MichaelM3 <michael.muniz94@gmail.com>
2023-03-04 01:50:47 -0500

    temporarly finished models file and set up relationships

---
+++ app/models.py
@@ -0,0 +1,30 @@
+import uuid
+from sqlalchemy import Boolean, Integer, String, Column, ForeignKey, Table
+from sqlalchemy.orm import relationship
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String(36), primary_key=True, default=str(uuid.uuid4()), index=True)
+    username = Column(String(15), unique=True, nullable=False, index=True)
+    email = Column(String(50), unique=True, nullable=False)
+    password = Column(String(40), nullable=False)
+    image = Column(String(200), default="")
+    recipes = relationship("Recipe", secondary="user_recipe_assocation_table", back_populates="users")
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(String(36), primary_key=True, default=str(uuid.uuid4()), index=True)
+    title = Column(String(), nullable=False, index=True)
+    summary = Column(String(), default="No summary given")
+    image = Column(String(), default="")
+    spoonacularId = Column(String(), nullable=False)
+    users = relationship("User", secondary="user_recipe_assocation_table", back_populates="recipes")
+
+user_recipe_assocation_table = Table("user_recipe_assocation_table", Base.metadata, 
+    Column("user_id", String(36), ForeignKey("user_id"), primary_key=True),
+    Column("recipe_id", String(36), ForeignKey("recipe_id"), primary_key=True)
+)
+
