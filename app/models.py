3313d3674941cf1791bae58606e932a10c491d19
MichaelM3 <michael.muniz94@gmail.com>
2023-03-04 22:30:19 -0500

    fixed models and new migration

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Integer, String, Column, ForeignKey, Table
+from sqlalchemy import Integer, String, Column, ForeignKey, Table, null
 from sqlalchemy.orm import relationship
 from app.database import Base
 
@@ -10,8 +10,8 @@ class User(Base):
     username = Column(String(15), unique=True, nullable=False, index=True)
     email = Column(String(50), unique=True, nullable=False)
     password = Column(String(40), nullable=False)
-    image = Column(String(200), default="")
-    recipes = relationship("Recipe", secondary="user_recipe_assocation_table", back_populates="users")
+    image = Column(String(200), nullable=True)
+    recipes = relationship("Recipe", secondary="user_recipe_association_table", back_populates="users")
 
 class Recipe(Base):
     __tablename__ = "recipes"
@@ -19,12 +19,12 @@ class Recipe(Base):
     id = Column(String(36), primary_key=True, default=str(uuid.uuid4()), index=True)
     title = Column(String(), nullable=False, index=True)
     summary = Column(String(), default="No summary given")
-    image = Column(String(), default="")
-    spoonacularId = Column(Integer, nullable=False)
-    users = relationship("User", secondary="user_recipe_assocation_table", back_populates="recipes")
+    image = Column(String(), nullable=True)
+    spoonacular_id = Column(Integer, nullable=False)
+    users = relationship("User", secondary="user_recipe_association_table", back_populates="recipes")
 
-user_recipe_assocation_table = Table("user_recipe_assocation_table", Base.metadata, 
-    Column("user_id", String(36), ForeignKey("users.id"), primary_key=True),
-    Column("recipe_id", String(36), ForeignKey("recipes.id"), primary_key=True)
+user_recipe_association_table = Table("user_recipe_association_table", Base.metadata, 
+    Column("user_id", String(36), ForeignKey("users.id")),
+    Column("recipe_id", String(36), ForeignKey("recipes.id"))
 )
 
